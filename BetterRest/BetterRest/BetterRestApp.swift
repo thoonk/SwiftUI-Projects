ef082edfeda792f81df04392b72862e44b23e272
thoonk <dkrmrm878@naver.com>
2024-01-09 16:31:23 +0900

    추가: BetterRest (CoreML)

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by thoonk on 2024/01/09.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
