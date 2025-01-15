93a04474ce1e2952724a01ca391b218e51a47347
thoonk <dkrmrm878@naver.com>
2025-01-15 11:50:23 +0900

    추가: Navigation (NavigationStack, NavigationLink)

---
+++ Navigation/Navigation/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by thoonk on 2024/01/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var number: Int
+
+    var body: some View {
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                ToolbarItem(placement: .navigation) {
+                    Button("Tap Me") {
+                        
+                    }
+                }
+            }
+    }
+}
+
+struct ContentView: View {
+    @State private var pathStore = PathStore()
+
+    var body: some View {
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i)
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+
+        // Still here? Start with an empty path.
+        path = NavigationPath()
+    }
+
+    func save() {
+        guard let representation = path.codable else { return }
+
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by thoonk on 2024/01/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
